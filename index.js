3413b4fb548ed64b2993d9796d9f23847feac143
Max Kaplan <kaplanmaxe3@gmail.com>
2017-10-16 20:19:05 -0400

    adding preset

---
+++ index.js
@@ -0,0 +1,19 @@
+module.exports = {
+  presets: [
+    [
+      require('babel-preset-env'), {
+        browsers: "last 2 versions"
+      }
+    ],
+    [ require('babel-preset-react') ]
+  ],
+  plugins: [
+    [
+      require('babel-plugin-transform-runtime'), {
+        polyfill: false
+      }
+    ],
+    [ require('babel-plugin-transform-class-properties') ],
+    [ require('babel-plugin-transform-object-rest-spread') ]
+  ]
+};
